871803ec304e2fce6dd4c1a766b53e00b912dcec
ViniciusBorges <viniciusborges22@gmail.com>
2017-11-08 12:53:16 -0200

    adicionado struct do indice

--- arvore_b.h
+++ arvore_b.h
@@ -1,6 +1,14 @@
 #ifndef ARVORE_B_H_INCLUDED
 #define ARVORE_B_H_INCLUDED
 
+#define ORDEM 5 //ordem da árvore
 
+typedef struct
+{
+    int tam;    //quantidade de chaves armazenadas no nó
+    int chaves[ORDEM-1];    //valor das chaves
+    int ponteiros[ORDEM];   //ponteiros para os filhos do nó
+}
+no;
 
 #endif // ARVORE_B_H_INCLUDED
